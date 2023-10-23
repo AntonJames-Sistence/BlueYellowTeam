857c4b5d299d20f94973aa4419c25c2fca63d5c2
Alex <alex.j.basso@gmail.com>
2023-10-23 15:37:50 -0500

    footer spacing

--- client/src/components/Footer/footer.jsx
+++ client/src/components/Footer/footer.jsx
@@ -19,38 +19,40 @@ export default function Footer() {
   }
 
   return (
-    <div id="footer-cont">
-      <div id="footer-left">
-        <img id="byf-logo" src="../../public/byf-logo.svg" />
-        <span id="legal">Blue and Yellow is a 501(c)3 registered non-profit organization. Please reach out to ola@blueyellowfoundation.org to receive your donation receipt for tax deduction purposes.</span>
-        <div id="social-links">
-          <a className="social-link" href="https://www.facebook.com/BlueYellowFoundation"> <i className=" fa-brands fa-facebook-f" />FACEBOOK</a>
-          <a className="social-link" href="https://www.instagram.com/blueyellowfoundation/"> <i className=" fab fa-instagram"></i> INSTAGRAM</a>
-          <a className="social-link" href="https://www.gofundme.com/f/vvyptk-support-ukraine"> <i className=" fas fa-piggy-bank"></i> Gofundme</a>
+    <div id="footer-wrapper">
+      <div id="footer-cont">
+        <div id="footer-left">
+          <img id="byf-logo" src="../../public/byf-logo.svg" />
+          <span id="legal">Blue and Yellow is a 501(c)3 registered non-profit organization. Please reach out to ola@blueyellowfoundation.org to receive your donation receipt for tax deduction purposes.</span>
+          <div id="social-links">
+            <a className="social-link" href="https://www.facebook.com/BlueYellowFoundation"> <i className=" fa-brands fa-facebook-f" />FACEBOOK</a>
+            <a className="social-link" href="https://www.instagram.com/blueyellowfoundation/"> <i className=" fab fa-instagram"></i> INSTAGRAM</a>
+            <a className="social-link" href="https://www.gofundme.com/f/vvyptk-support-ukraine"> <i className=" fas fa-piggy-bank"></i> Gofundme</a>
+          </div>
+          <span id="copyright">© Blue & Yellow Foundation</span>
         </div>
-        <span id="copyright">© Blue & Yellow Foundation</span>
-      </div>
-      <div id="newsletter-signup">
-        <form id="newsletter-form" onSubmit={handleSubmit}>
-        <span id="newsletter-header">NEWSLETTER</span>
-          <input
-            type="text"
-            placeholder="Name"
-            onChange={e => setName(e.target.value)}
-            value={name}
-            required
-          />
-          <input
-            type="text"
-            placeholder="Email"
-            onChange={e => setEmail(e.target.value)}
-            value={email}
-            required
-          />
-          <button>SUBSCRIBE</button>
+        <div id="newsletter-signup">
+          <form id="newsletter-form" onSubmit={handleSubmit}>
+            <span id="newsletter-header">NEWSLETTER</span>
+            <input
+              type="text"
+              placeholder="Name"
+              onChange={e => setName(e.target.value)}
+              value={name}
+              required
+            />
+            <input
+              type="text"
+              placeholder="Email"
+              onChange={e => setEmail(e.target.value)}
+              value={email}
+              required
+            />
+            <button>SUBSCRIBE</button>
 
-          {submitted && !name.length && !email.length ? <span id="signup-thanks">Thank you! You have successfully subscribed.</span> : null}
-        </form>
+            {submitted && !name.length && !email.length ? <span id="signup-thanks">Thank you! You have successfully subscribed.</span> : null}
+          </form>
+        </div>
       </div>
     </div>
   )
