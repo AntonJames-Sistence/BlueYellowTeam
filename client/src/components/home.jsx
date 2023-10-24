5da2a2530bfe3cccd2b691eb099fab798a544057
AntonJames-Sistence <anton.james.ja@gmail.com>
2023-10-24 11:26:24 -0400

    Events component

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -1,16 +1,12 @@
 import { Link } from "react-router-dom";
-import FeaturedProjects from "./FeaturedProjects";
-import About from "./About";
-import About2 from "./About2";
-import FacebookLogs from "./FacebookLogs";
 
 export default function HomePage() {
-  return (
-    <div className="text-black pt-20">
-      <About />
-      <FeaturedProjects />
-      <About2 />
-      <FacebookLogs />
-    </div>
-  );
+
+    return(
+    <>
+        <h1>Carlos rules</h1>
+        <Link to="vite">To Vite</Link>
+    </>
+    ) 
+    
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,34 +1,33 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App.jsx";
-import "./index.css";
-import HomePage from "./components/home.jsx";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Vite from "./components/vite.jsx";
-import ErrorPage from "./components/error-page.jsx";
-import NavBarWrapper from "./components/NavBarWrapper/NavBarWrapper.tsx";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App.jsx'
+import './index.css'
+import HomePage from './components/home.jsx';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Vite from './components/vite.jsx';
+import ErrorPage from './components/error-page.jsx';
+import Events from './components/Events/Events.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <NavBarWrapper />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "/",
-        element: <HomePage />,
-      },
-    ],
+    element: <HomePage />,
+    errorElement: <ErrorPage/>,
   },
   {
-    path: "/vite",
-    element: <Vite />,
-    errorElement: <ErrorPage />,
+    path:"/vite",
+    element: <Vite/>,
+    errorElement: <ErrorPage/>,
+  },
+  {
+    path:"/events",
+    element: <Events />,
+    errorElement: <ErrorPage/>,
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
