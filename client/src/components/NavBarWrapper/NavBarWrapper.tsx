c3a1e1da1c0a7f58da75dddd5c732b560ca628fb
James <james.hernandez1225@gmail.com>
2023-10-24 12:33:31 -0400

    refactored merge

--- client/src/components/NavBarWrapper/NavBarWrapper.tsx
+++ client/src/components/NavBarWrapper/NavBarWrapper.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import NavBar from "../NavBar/NavBar";
 import { Outlet } from "react-router-dom";
-
+import Footer from "../Footer/footer";
 const NavBarWrapper = () => {
   return (
     <div>
       <NavBar />
       <Outlet />
+      <Footer />
     </div>
   );
 };
--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -3,11 +3,13 @@ import FeaturedProjects from "./FeaturedProjects";
 import About from "./About";
 import About2 from "./About2";
 import FacebookLogs from "./FacebookLogs";
+import Events from "./Events/Events";
 
 export default function HomePage() {
   return (
-    <div className="text-black pt-20">
+    <div id="Home" className="text-black pt-20">
       <About />
+      <Events />
       <FeaturedProjects />
       <About2 />
       <FacebookLogs />
