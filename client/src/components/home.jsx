3cb55210939e78e1afc3b24dc703c90c23e8557f
Anton James <126339704+AntonJames-Sistence@users.noreply.github.com>
2023-10-24 11:45:41 -0400

    Revert "Dev"

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
+import FeaturedProjects from "./FeaturedProjects";
+import About from "./About";
+import About2 from "./About2";
+import FacebookLogs from "./FacebookLogs";
 
 export default function HomePage() {
-
-    return(
-    <>
-        <h1>Carlos rules</h1>
-        <Link to="vite">To Vite</Link>
-    </>
-    ) 
-    
+  return (
+    <div className="text-black pt-20">
+      <About />
+      <FeaturedProjects />
+      <About2 />
+      <FacebookLogs />
+    </div>
+  );
 }
