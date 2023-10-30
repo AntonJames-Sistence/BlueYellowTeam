8df5a765e46188cc1d52a9026bb3c1b7c46892f5
lokisk1155 <shawn12345d@gmail.com>
2023-10-30 08:36:58 -0400

    pass build

--- app/api/stripe/route.js
+++ app/api/stripe/route.js
@@ -1,22 +1,24 @@
-// This is your test secret API key.
-const stripe = require('stripe')(process.env.STRIPE_TEST);
+import { NextResponse } from 'next/server';
 
-export async function POST(req, res) {
+const stripe = process.env.STRIPE_TEST;
+
+export async function POST() {
   const url = '/create-checkout-session';
-  const session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-        price: '{{PRICE_ID}}',
-        quantity: 1,
-      },
-    ],
-    mode: 'payment',
-    success_url: `${YOUR_DOMAIN}/success.html`,
-    cancel_url: `${YOUR_DOMAIN}/cancel.html`,
-  });
-
-  res.redirect(303, session.url);
+  try {
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+          price: '{{PRICE_ID}}',
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: `${YOUR_DOMAIN}/success.html`,
+      cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+    });
+    NextResponse.redirect(303, session.url);
+  } catch (error) {
+    return NextResponse.error();
+  }
 }
-
-app.listen(4242, () => console.log('Running on port 4242'));
