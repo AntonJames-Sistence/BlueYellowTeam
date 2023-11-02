406f3542fdf77560f6ffe15b52abd1084cd35935
Alex <alex.j.basso@gmail.com>
2023-11-02 11:56:15 -0500

    youtube block

--- app/components/youtube.js
+++ app/components/youtube.js
@@ -5,13 +5,8 @@ const Youtube = () => {
   return (
     <div
       id="About"
-      className=" w-full flex flex-col pt-36 lg:grid lg:grid-cols-2 lg:grid-rows-1"
+      className=" w-full flex flex-col mt-28 lg:grid lg:grid-cols-2 lg:grid-rows-1 bg-darkTeal p-16"
     >
-      <YoutubeEmbed
-        idk="w-full h-100 lg:h-full lg:mr-11 rounded-2xl"
-        wrapper="lg:mr-11"
-        embedId="qpWkGYq-cmE"
-      />
       <div className="text-sky text-2xl lg:pr-8 pb-14 lg:pl-11 tracking-normal leading-normal py-6 lg:p-14">
         <h3>
           We are in the middle of the worst European humanitarian crisis since
@@ -20,6 +15,15 @@ const Youtube = () => {
           501c3 certified, and 100% of every donation is tax deductible.
         </h3>
       </div>
+      <YoutubeEmbed
+        idk="w-full h-100 lg:h-full lg:mr-11 rounded-2xl"
+        wrapper="lg:mr-11"
+        embedId="qpWkGYq-cmE"
+      />
+
+
+
+
     </div>
   );
 };
