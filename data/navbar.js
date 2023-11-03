67894aa46fc060415a0c1c8c0418cbb3a2a1a424
Shawn Mallon <95663040+lokisk1155@users.noreply.github.com>
2023-11-03 12:35:06 -0400

    Home Page Css Updates (#24)
    * cool
    
    * nice

--- data/navbar.js
+++ data/navbar.js
@@ -1,9 +1,6 @@
 export const navLinks = [
-  { href: '', title: 'Home' },
-  { href: '/donate', title: 'Donate' },
-  { href: '#EventsAndProjects', title: 'Events & Projects' },
-  { href: '#Causes', title: 'Causes' },
-  { href: '#Team', title: 'Team' },
-  { href: '#Contacts', title: 'Contacts' },
+  { href: '#who-we-help', title: 'Causes' },
+  { href: '#events', title: 'Events' },
+  { href: '#projects', title: 'Projects' },
   { href: '/donate', title: 'Donate Now' },
 ];
