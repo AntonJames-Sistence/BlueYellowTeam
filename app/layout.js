4837bd0b67d6bd3d3b634e3454d7bbb04e276704
lokisk1155 <shawn12345d@gmail.com>
2023-11-02 21:43:57 -0400

    cool

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
 import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -22,6 +23,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <NavBar />
         {children}
+        <Footer />
       </body>
     </html>
   );
