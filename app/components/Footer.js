0c290e6280b02fa4ae41a2e23715dcbe953e3d62
Shawn Mallon <95663040+lokisk1155@users.noreply.github.com>
2023-11-02 22:22:27 -0400

    Non-interactive Footer (#22)
    * non interactive footer
    
    * clean css for footer

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,3 +1,34 @@
+// "use client"
+import FooterForm from './FooterForm';
+
 export default function Footer() {
-  return <footer>temp</footer>;
+  return (
+    <footer className="w-full bg-darkTeal text-white">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex flex-col sm:flex-row justify-between items-center py-4">
+          <div className="w-full md:w-1/2 flex items-center mb-4 sm:mb-0">
+            <img
+              src="/blue-yellow-logo.png"
+              alt="Blue and Yellow Logo"
+              className="h-20 w-20 object-contain mr-4"
+            />
+            <div>
+              <div className="font-semibold tracking-wider mb-2">
+                Blue and Yellow
+              </div>
+              <p className="text-xs">
+                &copy; Blue and Yellow is a 501(c)3 registered non-profit
+                organization. Please reach out to ola@blueyellowfoundation.org
+                to receive your donation receipt for tax deduction purposes.
+              </p>
+            </div>
+          </div>
+
+          <div className="w-full md:w-1/2">
+            <FooterForm />
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
