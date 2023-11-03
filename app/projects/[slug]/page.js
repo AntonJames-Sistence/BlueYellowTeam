f2b62d3d04dfcedcd2b9bedb96d4c3e45c125189
lokisk1155 <shawn12345d@gmail.com>
2023-11-03 13:37:32 -0400

    projects from mock data + project slug page

---
+++ app/projects/[slug]/page.js
@@ -0,0 +1,13 @@
+import { allProjects } from '../../../data/projects';
+
+export default function ProjectPage({ params: { slug } }) {
+  const project = allProjects.find((project) => project.slug === slug);
+  return (
+    <div className="pt-24">
+      <div className="p-5">
+        <div className="text-3xl mt-0 mb-2 text-gray-700">{project.title}</div>
+        <div className="text-3xl mt-0 mb-2 text-gray-700">{project.para}</div>
+      </div>
+    </div>
+  );
+}
