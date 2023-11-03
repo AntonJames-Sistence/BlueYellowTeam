4f8893b0156f9445d5214f0e5a5647cece7e3435
lokisk1155 <shawn12345d@gmail.com>
2023-11-03 13:40:36 -0400

    min heights

--- app/projects/[slug]/page.js
+++ app/projects/[slug]/page.js
@@ -3,7 +3,7 @@ import { allProjects } from '../../../data/projects';
 export default function ProjectPage({ params: { slug } }) {
   const project = allProjects.find((project) => project.slug === slug);
   return (
-    <div className="pt-24">
+    <div className="pt-24 min-h-[100vh]">
       <div className="p-5">
         <div className="text-3xl mt-0 mb-2 text-gray-700">{project.title}</div>
         <div className="text-3xl mt-0 mb-2 text-gray-700">{project.para}</div>
