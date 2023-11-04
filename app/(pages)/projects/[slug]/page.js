ebf2daa153911bf43447d3af1b553ab737e7b59f
lokisk1155 <shawn12345d@gmail.com>
2023-11-04 14:32:55 -0400

    forgot import

--- app/(pages)/projects/[slug]/page.js
+++ app/(pages)/projects/[slug]/page.js
@@ -1,4 +1,5 @@
 import { allProjects } from "../../../../data/projects";
+import PageHeader from "../../components/PageHeader";
 
 export default function ProjectIndexPage({ params: { slug } }) {
   const project = allProjects.find((project) => project.slug === slug);
