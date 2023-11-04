5551109947c1d5c3e6e839c4f13f00b877dbaa63
AntonJames-Sistence <anton.james.ja@gmail.com>
2023-11-04 19:23:03 -0400

    Add minor bug fixes

--- app/components/Banner.js
+++ app/components/Banner.js
@@ -12,17 +12,19 @@ const Banner = () => {
                 controls={false}
             ></video>
             <div className="absolute top-0 left-0 w-full h-full flex flex-col md:flex-row">
-                <div className="w-full lg:-mt-22 md:-mt-12 md:w-1/2 flex flex-col justify-center p-5">
-                    <div className="text-3xl md:text-4xl xl:text-6xl text-white font-bold text-shadow-lg md:pb-6">
-                        STAND WITH LOVE STAND WITH UKRAINE
+                <div className="w-full lg:-mt-16 md:-mt-20 md:w-1/2 flex flex-col justify-center p-5">
+                    <div className="text-2xl md:text-4xl xl:text-5xl text-white font-bold text-shadow-lg md:pb-6">
+                        STAND WITH LOVE, STAND WITH UKRAINE
                     </div>
-                    <div className="text-white lg:text-left font-bold text-base md:text-lg xl:text-xl lg:pt-8">
-                        100% OF PROFIT GOES TO SUPPORT UKRAINIANS IN NEED
+                    <div className="text-white lg:text-left font-bold text-base md:text-lg xl:text-xl">
+                        100% OF THE PROFITS GO <br/> TO SUPPORT UKRAINIANS IN NEED
                     </div>
                 </div>
                 <div
-                    className="w-full md:w-1/2 h-full bg-cover bg-no-repeat rounded-3xl"
-                    style={{ backgroundImage: `url(./intro_banner.png)` }}
+                    className="w-full md:w-3/4 sm:hidden h-full bg-cover bg-no-repeat rounded-3xl lg:block md:block"
+                    style={{ 
+                            backgroundImage: `url(./intro_banner.png)`,
+                    }}
                 ></div>
             </div>
         </div>
