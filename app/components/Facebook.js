95a369b7be581eafc2f8f2b9c947950673abf239
James <james.hernandez1225@gmail.com>
2023-11-04 12:48:57 -0400

    removing facebook changes

--- app/components/Facebook.js
+++ app/components/Facebook.js
@@ -24,10 +24,6 @@ export default function Facebook() {
       const shrunkFbList = facebookData.slice(0, numOfPost);
       let tempFacebookColumns = [[], [], []];
 
-      // if (window.innerWidth >= 1000) tempFacebookColumns = [[], [], []];
-      // else if (window.innerWidth >= 700) tempFacebookColumns = [[], []];
-      // else tempFacebookColumns = [[]];
-
       //divides facebookData into three columns to make layout/css easier
       for (let i = 0; i < shrunkFbList.length; i++) {
         const index = i % tempFacebookColumns.length;
