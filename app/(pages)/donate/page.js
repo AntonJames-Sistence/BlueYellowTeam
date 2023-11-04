d8649012d05a6c2458e0501b7c63f570a76e9054
AntonJames-Sistence <anton.james.ja@gmail.com>
2023-11-04 17:27:02 -0400

    Fix style Donate page

--- app/(pages)/donate/page.js
+++ app/(pages)/donate/page.js
@@ -3,11 +3,12 @@ import PageHeader from '../components/PageHeader';
 
 export default function DonatePage() {
   return (
-    <div className="flex flex-col items-center justify-start p-5">
-      <PageHeader title={'Why Donate'} />
-      <div className="w-full pt-24 min-h-[100vh]">
+    <div className="flex flex-col items-center justify-start">
+      {/* <PageHeader title={'Who do we help'} /> */}
+      <h1 className='text-5xl'>Who do we help?</h1>
+      {/* <div className="w-full pt-24 min-h-[100vh]"> */}
         <Donate />
-      </div>
+      {/* </div> */}
     </div>
   );
 }
