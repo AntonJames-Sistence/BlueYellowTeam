9bac9ed888d3f7c016ac635178094909cd3b618a
AntonJames-Sistence <anton.james.ja@gmail.com>
2023-11-05 15:46:27 -0500

    Add responsive style to Youtube component, Add change to Banner component style

--- app/components/Banner.js
+++ app/components/Banner.js
@@ -4,26 +4,26 @@ import Link from 'next/link';
 const Banner = () => {
     return (
         <>
-            <div className="relative h-[400px] w-full">
+            <div className="relative h-[400px] w-screen">
                 <video
-                    className="absolute top-0 left-0 w-full h-full object-cover z-0 rounded-3xl"
+                    className="absolute top-0 left-0 w-full h-full object-cover z-0 "
                     src="./flag_bg.mp4"
                     autoPlay
                     loop
                     muted
                     controls={false}
                 ></video>
-                <div className="absolute top-0 left-0 w-full h-full flex flex-col md:flex-row">
+                <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-between md:flex-row">
                     <div className="w-full lg:-mt-16 md:-mt-20 md:w-1/2 flex flex-col justify-center p-5">
                         <div className="text-2xl md:text-4xl xl:text-5xl text-white font-bold text-shadow-lg md:pb-6">
                             STAND WITH LOVE, STAND WITH UKRAINE
                         </div>
-                        <div className="text-white lg:text-left font-bold text-base md:text-lg xl:text-xl">
+                        <div className="text-white md:-mb-6 lg:text-left font-bold text-sm mt-4 md:text-lg xl:text-xl">
                             100% OF THE PROFITS GO <br/> TO SUPPORT UKRAINIANS IN NEED
                         </div>
                     </div>
                     <div
-                        className="w-full md:w-3/4 sm:hidden h-full bg-cover bg-no-repeat rounded-3xl lg:block md:block"
+                        className="w-full md:w-3/4 h-1/2 md:h-full lg:h-full bg-cover bg-no-repeat"
                         style={{ 
                                 backgroundImage: `url(./intro_banner.png)`,
                         }}
--- app/components/youtube.js
+++ app/components/youtube.js
@@ -3,18 +3,14 @@ import YoutubeEmbed from '../helpers/youtube';
 
 const Youtube = () => {
   return (
-    <div id="About"
-          className="w-screen bg-darkTeal min-h-[400px] mt-36 p-10">
+    <div id="About" className="w-screen bg-darkTeal mt-36 p-2 md:p-5 md:py-10 lg:p-10">
 
-      <div className="flex flex-row justify-evenly items-center">
-        <div className="md:w-1/3 text-white text-2xl font-bold">
-          We are in the middle of the worst European humanitarian crisis 
-          since World War II. The Blue & Yellow Foundation supports those 
-          affected by the war in Ukraine with humanitarian aid. We are 
-          volunteer-driven, 501c3 certified, and 100% of every donation is tax deductible.
+      <div className="flex flex-col justify-evenly md:justify-between items-center lg:flex-row md:flex-row">
+        <div className="w-full lg:w-1/2 md:w-1/3 text-white text-sm md:text-base lg:text-2xl font-bold p-4 md:p-0">
+          We are in the middle of the worst European humanitarian crisis since World War II. The Blue & Yellow Foundation supports those affected by the war in Ukraine with humanitarian aid. We are volunteer-driven, 501c3 certified, and 100% of every donation is tax deductible.
         </div>
         <YoutubeEmbed
-          idk="w-full h-full lg:min-h-[400px] md:min-w-[400px] lg:w-[700px] rounded-2xl"
+          idk="w-screen md:w-full p-4 h-[220px] md:w-[500px] md:h-[300px] lg:w-[800px] lg:h-[400px] lg:rounded-3xl"
           wrapper=""
           embedId="qpWkGYq-cmE"
         />
