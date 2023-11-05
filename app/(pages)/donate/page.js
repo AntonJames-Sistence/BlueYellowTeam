6fa078febde66bf376019df0764e1f13e5fe3b2c
AntonJames-Sistence <anton.james.ja@gmail.com>
2023-11-04 21:47:41 -0400

    Return Donate header, adjust WarClock

--- app/(pages)/donate/page.js
+++ app/(pages)/donate/page.js
@@ -4,11 +4,11 @@ import PageHeader from '../components/PageHeader';
 export default function DonatePage() {
   return (
     <div className="flex flex-col items-center justify-start">
-      {/* <PageHeader title={'Who do we help'} /> */}
-      <h1 className='text-4xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-6xl'>Who do we help?</h1>
-      {/* <div className="w-full pt-24 min-h-[100vh]"> */}
+      <PageHeader title={'Our Causes'} />
+      {/* <h1 className='text-3xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-5xl'>Who do we help?</h1> */}
+      <div className="w-full min-h-[100vh]">
         <Donate />
-      {/* </div> */}
+      </div>
     </div>
   );
 }
