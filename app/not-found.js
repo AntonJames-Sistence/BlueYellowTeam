9ba65f8b847587022a97f6015cd33cc9df13d00e
lokisk1155 <shawn12345d@gmail.com>
2023-11-06 00:15:59 -0500

    fix regular text build error whoops

--- app/not-found.js
+++ app/not-found.js
@@ -10,14 +10,12 @@ export default function NotFound() {
       </div>
       <div className="max-w-md">
         <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">
-          Sorry we couldn't find this page.
+          {"Sorry, we couldn't find this page."}
         </p>
-        <p className="mb-8">But dont worry, we have plently of fish for you!</p>
-        <Link
-          href="/"
-          className="focus:shadow-outline-blue inline rounded-lg border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500"
-        >
-          Back to home
+        <Link href="/">
+          <a className="focus:shadow-outline-blue inline rounded-lg border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500">
+            Back to home
+          </a>
         </Link>
       </div>
     </div>
