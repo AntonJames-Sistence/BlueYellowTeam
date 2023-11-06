5789ca54df3044bd1a52ed001ef57e37f796a629
James <james.hernandez1225@gmail.com>
2023-11-06 00:11:23 -0500

    fixed project and events page responsiveness

--- app/(pages)/events/page.js
+++ app/(pages)/events/page.js
@@ -1,11 +1,11 @@
-import HomeEvents from '../../components/HomeEvents';
-import PageHeader from '../components/PageHeader';
+import HomeEvents from "../../components/HomeEvents";
+import PageHeader from "../components/PageHeader";
 
 export default function EventsPage() {
   return (
     <div className="flex flex-col items-center justify-start">
-      <PageHeader title={'Our Events'} />
-      <div className=" min-h-[100vh]">
+      <PageHeader title={"Our Events"} />
+      <div className=" min-h-[100vh] w-full">
         <HomeEvents />
       </div>
     </div>
