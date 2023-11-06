2d6ab16a40dc350eea46c19b9fa4c68bbb83619d
James <james.hernandez1225@gmail.com>
2023-11-06 11:26:28 -0500

    updated events imports

--- app/(pages)/events/page.js
+++ app/(pages)/events/page.js
@@ -2,11 +2,8 @@ import PastUpcomingEvents from "../../components/PastUpcomingEvents";
 
 export default function EventsPage() {
   return (
-    <div className="flex flex-col items-center justify-start">
-      <PageHeader title={"Our Events"} />
-      <div className=" min-h-[100vh] w-full">
-        <HomeEvents />
-      </div>
+    <div className="min-h-[100vh] w-full">
+      <PastUpcomingEvents />
     </div>
   );
 }
