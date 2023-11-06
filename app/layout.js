e595ce8c34bd7868c43b288556815da0e9da8db7
James <james.hernandez1225@gmail.com>
2023-11-06 11:21:54 -0500

    updated footer import

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
 import NavBar from "./components/NavBar";
-import Footer from "./components/Footer";
+import Footer from ".././components/Footer";
 import { getServerSession } from "next-auth";
 import SessionProvider from "./components/SessionProvider";
 
