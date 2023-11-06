9d6b35120b1c1f8b3916e6289ab9a8b3e15a2a01
James <james.hernandez1225@gmail.com>
2023-11-06 18:54:59 -0500

    hard coded projectId

--- data/firebase.js
+++ data/firebase.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
+  projectId: "blueyellowfoundation-b5284",
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSENGER_SEND,
   appId: process.env.FIREBASE_APP_ID,
