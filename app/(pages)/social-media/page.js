4aa9ea9aaab4294ff1ffed7071cb1a81b39db6bf
lokisk1155 <shawn12345d@gmail.com>
2023-11-06 07:47:47 -0500

    nice

--- app/(pages)/social-media/page.js
+++ app/(pages)/social-media/page.js
@@ -2,7 +2,7 @@ import Facebook from '../../components/Facebook';
 
 export default function EventsPage() {
   return (
-    <div className="pt-24 min-h-[100vh]">
+    <div className="min-h-[100vh] w-full">
       <Facebook />
     </div>
   );
