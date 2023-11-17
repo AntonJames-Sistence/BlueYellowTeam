8ced46dac0a6a52b48c7d45209775fb7086375e4
AntonJames-Sistence <anton.james.ja@gmail.com>
2023-11-17 09:47:12 -0500

    Final style for buttons

--- app/components/Banner.js
+++ app/components/Banner.js
@@ -32,9 +32,10 @@ const Banner = () => {
         </div>
       </div>
       <div className="flex flex-row w-11/12 justify-between max-w-[1400px] mt-4">
-                <Link href="./donate" className='w-2/6 md:w-1/5'>
+                <Link href="./donate" className='w-[85px] md:w-1/5 flex flex-row'>
                   <button className="
-                          h-12
+                          h-full
+                          md:h-12
                           w-full
                           bg-yellow-400
                           hover:bg-blue-400
@@ -47,62 +48,77 @@ const Banner = () => {
                           justify-center
                           ease-in-out duration-300
                           px-4
-                          text-sm
-                          "><i className="fa-brands fa-stripe fa-2xl mr-2" ></i>Donate with Stripe
+                          text-xs
+                          md:text-sm
+                          "><i className="fa-brands fa-cc-stripe fa-xl md:mr-6"></i>
+                          <div className="inline">Donate with Stripe</div>
                   </button>
                 </Link>
-                <Link href="./donate" className='w-2/6 md:w-1/5'>
+                <Link href="https://www.paypal.com/donate/?hosted_button_id=6S6S2484WWCKN" target="_blank" rel="noopener noreferrer" className='w-[85px] md:w-1/5'>
                   <button className="
-                          h-12
+                          h-full
+                          md:h-12
                           w-full
                           bg-yellow-400
                           hover:bg-blue-400
                           text-black
                           hover:text-white
                           hover:scale-110
-                          font-medium
+                          font-bold
                           rounded-2xl
                           items-center
                           justify-center
                           ease-in-out duration-300
                           px-4
-                          ">Donate now
+                          text-xs
+                          md:text-sm
+                          "><i className="fa-brands fa-paypal fa-xl md:mr-6"></i>
+                          <div className="inline">Donate with PayPal</div>
                   </button>
                 </Link>
-                <Link href="./donate" className='w-2/6 md:w-1/5'>
+                <Link href="./donate" className='w-[85px] md:w-1/5'>
                   <button className="
-                          h-12
+                          h-full
+                          w-full
+                          md:h-12
+                          md:w-full
+                          bg-yellow-400
+                          hover:bg-blue-400
+                          text-black
+                          hover:text-white
+                          hover:scale-110
+                          font-bold
+                          rounded-2xl
+                          items-center
+                          justify-center
+                          ease-in-out duration-300
+                          px-4
+                          text-xs
+                          md:text-sm
+                          "><i className="fa-brands fa-google-pay fa-2xl md:mr-6"></i>
+                          <div className="md:inline">Donate with GooglePay</div>
+                  </button>
+                </Link>
+                <Link href="./donate" className='w-[85px] md:w-1/5'>
+                  <button className="
+                          h-full
+                          md:h-12
                           w-full
                           bg-yellow-400
                           hover:bg-blue-400
                           text-black
                           hover:text-white
                           hover:scale-110
-                          font-medium
+                          font-bold
                           rounded-2xl
                           items-center
                           justify-center
                           ease-in-out duration-300
                           px-4
-                          ">Donate now
-                  </button>
-                </Link>
-                <Link href="./donate" className='w-2/6 md:w-1/5'>
-                  <button className="
-                          h-12
-                          w-full
-                          bg-yellow-400
-                          hover:bg-blue-400
-                          text-black
-                          hover:text-white
-                          hover:scale-110
-                          font-medium
-                          rounded-2xl
-                          items-center
-                          justify-center
-                          ease-in-out duration-300
-                          px-4
-                          ">Donate now
+                          text-xs
+                          md:text-sm
+                          "><i class="fa-brands fa-apple-pay fa-2xl md:mr-6"></i>
+                          <div className="inline">Donate with ApplePay</div>
                   </button>
                 </Link>
                 
