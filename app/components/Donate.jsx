7b272eed2dc1d21851a6fd5903f2074e5cfba75f
AntonJames-Sistence <anton.james.ja@gmail.com>
2023-11-29 11:45:52 -0500

    Add react-transition library for smooth animation

--- app/components/Donate.jsx
+++ app/components/Donate.jsx
@@ -1,11 +1,12 @@
 'use client';
 
 import React, { useState, useEffect } from 'react';
+import { CSSTransition } from 'react-transition-group';
 import axios from 'axios';
 import "./main.css";
 
 const Donate = () => {
-    const [currentQuestion, setCurrentQuestion] = useState(2);
+    const [currentQuestion, setCurrentQuestion] = useState(1);
 
     const [cause, setCause] = useState('');
     const [trackAmount, setTrackAmount] = useState(null);
@@ -17,6 +18,7 @@ const Donate = () => {
     const [loading, setLoading] = useState(false);
 
     useEffect(() => {
+        console.log(method, amount)
         if (method === 'Card' && amount !== null) {
             handleStripeCheckout();
         }
@@ -24,21 +26,24 @@ const Donate = () => {
 
     const causes = [
         {
+            id: 1,
             text: 'Children',
             image: './causes-children.jpeg'
         },
         {
+            id: 2,
             text: 'Medical Workers',
             image: './causes-medical-2.jpeg'
         },
         {
+            id: 3,
             text: 'Displaced People',
             image: './causes-displaced.jpeg'
         },
     ]
 
     const causeChoice = (
-        <div className='mt-8 flex flex-row justify-center w-full'>
+        <div className='flex flex-row justify-center w-full'>
             {causes.map((cause) => (
                 <button
                     key={cause.id}
@@ -61,7 +66,7 @@ const Donate = () => {
     )
 
     const paymentChoice = (
-        <div className='w-full mt-8 flex flex-row justify-center'>
+        <div className='w-full flex flex-row justify-center'>
             <button className="bg-blue-500 text-white m-4 p-2 rounded-xl hover:bg-blue-600 hover:scale-110 ease-in-out duration-300 w-1/4 shadow-custom"
                     onClick={() => handleAnswerClick(3, 'Card')}>Card
             </button>
@@ -75,15 +80,15 @@ const Donate = () => {
     )
 
     const amountChoice = (
-        <div className='w-full mt-8 flex flex-row justify-center'>
+        <div className='w-full flex flex-row justify-center'>
             <button className="bg-blue-500 text-white m-4 p-2 rounded-xl hover:bg-blue-600 hover:scale-110 ease-in-out duration-300 w-1/4 shadow-custom"
-                    onClick={() => handleAnswerClick(1, 'Card')}>$20
+                    onClick={() => handleAnswerClick(1, 20)}>$20
             </button>
             <button className="bg-blue-500 text-white m-4 p-2 rounded-xl hover:bg-blue-600 hover:scale-110 ease-in-out duration-300 w-1/4 shadow-custom"
-                    onClick={() => handleAnswerClick(1, 'AppleGoogle')}>$50
+                    onClick={() => handleAnswerClick(1, 50)}>$50
             </button>
             <button className="bg-blue-500 text-white m-4 p-2 rounded-xl hover:bg-blue-600 hover:scale-110 ease-in-out duration-300 w-1/4 shadow-custom"
-                    onClick={() => handleAnswerClick(1, 'PayPal')}>$100
+                    onClick={() => handleAnswerClick(1, 100)}>$100
             </button>
             <div className='flex flex-row w-1/4 '>
                 <input
@@ -101,36 +106,6 @@ const Donate = () => {
         </div>
     )
 
-    // const questions = [
-    //     {
-    //         questionText: 'What cause do you want to donate?',
-    //         answerOptions: [
-    //             { answerText: childrenCause, nextQuestion: 1, answer: 'Children' },
-    //             { answerText: MWCause, nextQuestion: 1, answer: 'Medical Workers' },
-    //             { answerText: DPCause, nextQuestion: 1, answer: 'Displaced People' },
-    //         ],
-    //     },
-    //     {
-    //         questionText: 'What payment method?', // for now it is looped, change later
-    //         answerOptions: [
-    //             { answerText: 'Bank Card', nextQuestion: 2, answer: 'Stripe' },
-    //             { answerText: 'ApplePay / GooglePay', nextQuestion: 2, answer: 'Mobile' },
-    //             { answerText: 'PayPal', nextQuestion: 2, answer: 'PayPal' },
-    //         ],
-    //     },
-    //     {
-    //         questionText: 'Amount',
-    //         answerOptions: [
-    //             { answerText: '$20', nextQuestion: 0, answer: 20 },
-    //             { answerText: '$50', nextQuestion: 0, answer: 50 },
-    //             { answerText: '$100', nextQuestion: 0, answer: 100 },
-    //             { answerText: 'Custom', nextQuestion: 0, answer: null },
-    //         ],
-    //     },
-    // ];
-
-    // ===================================================
-
     const choices = [
         {
             id: 1,
@@ -141,24 +116,16 @@ const Donate = () => {
         {
             id: 2,
             choiceHeader: 'What payment method?',
-            dedicatedBlock: null,
+            dedicatedBlock: paymentChoice,
             nextChoiceId: 3,
         },
         {
             id: 3,
             choiceHeader: 'Please select amount',
-            dedicatedBlock: null,
-            nextChoiceId: 1,
-        },
-        {
-            id: 4,
-            choiceHeader: '',
-            dedicatedBlock: null,
+            dedicatedBlock: amountChoice,
             nextChoiceId: 1,
         },
     ]
-
-    // ===================================================
 
     const handleStripeCheckout = async () => {
         setLoading(true);
@@ -203,91 +170,66 @@ const Donate = () => {
         }
     };
 
+    const renderQuestion = () => {
+        switch (currentQuestion) {
+            case 1:
+                return (
+                    <CSSTransition
+                        in={currentQuestion === 1}
+                        timeout={500}
+                        classNames="slide-in-right"
+                        unmountOnExit
+                    >
+                        <div>
+                            <h2 className="text-center text-xl font-bold mt-4 mb-8">
+                                What cause do you want to donate?
+                            </h2>
+                            {causeChoice}
+                        </div>
+                    </CSSTransition>
+                );
+            case 2:
+                return (
+                    <CSSTransition
+                        in={currentQuestion === 2}
+                        timeout={500}
+                        classNames="slide-in-right"
+                        unmountOnExit
+                    >
+                        <div>
+                            <h2 className="text-center text-xl font-bold mt-4 mb-8">
+                                What payment method?
+                            </h2>
+                            {paymentChoice}
+                        </div>
+                    </CSSTransition>
+                );
+            case 3:
+                return (
+                    <CSSTransition
+                        in={currentQuestion === 3}
+                        timeout={500}
+                        classNames="slide-in-right"
+                        unmountOnExit
+                    >
+                        <div>
+                            <h2 className="text-center text-xl font-bold mt-4 mb-8">
+                                Please select amount
+                            </h2>
+                            {amountChoice}
+                        </div>
+                    </CSSTransition>
+                );
+            default:
+                return null;
+        }
+    };
+
     return (
-        amountChoice
+        <>
+            {renderQuestion()}
+        </>
     );
 };
 
 export default Donate;
-
-{/* <div className="p-4 w-full">
-            {loading ? (
-                <div className="flex items-center justify-center h-[80vh]">
-                    <div className="spinner"></div>
-                </div>
-            ) : 
-            (questions.map((question, index) => (
-                <div
-                    key={index}
-                    className={`w-full my-8 ${
-                    index === currentQuestion
-                        ? 'slide-in-right-enter slide-in-right-enter-active'
-                        : 'slide-in-right-exit slide-in-right-exit-active'
-                    } transition-opacity duration-700 ease-in-out`}
-                >
-                <h2 className="text-2xl mb-4 font-bold text-center">{question.questionText}</h2>
-                <div className="answer-options flex flex-row justify-center">
-                {question.answerOptions.map((option, optionIndex) => (
-                    <React.Fragment key={optionIndex}>
-                        {option.answerText === 'Custom' ? (
-                            <div className='flex flex-row w-1/4 '>
-                            <input
-                                type="number"
-                                value={trackAmount === null ? '' : trackAmount}
-                                onChange={(e) => setTrackAmount(e.target.value)}
-                                className="bg-white border border-gray-300 rounded-l-xl px-3 w-1/2 self-center h-[40px]"
-                                placeholder="Enter Amount"
-                            />
-                            <button
-                                className="bg-blue-500 text-white my-4 rounded-r-xl hover:bg-blue-600 w-1/2 min-h-[40px]"
-                                onClick={() => handleAnswerClick(option.nextQuestion, trackAmount)}
-                            >
-                                {option.answerText}
-                            </button>
-                            </div>
-                        ) : (
-                            <button
-                            key={optionIndex}
-                            className="bg-blue-500 text-white m-4 rounded-xl hover:bg-blue-600 hover:scale-110 ease-in-out duration-300 w-1/4 shadow-custom min-h-[40px]"
-                            onClick={() => handleAnswerClick(option.nextQuestion, option.answer)}
-                            >
-                            {option.answerText}
-                            </button>
-                        )}
-                    </React.Fragment>
-                ))}
-                </div>
-            </div>
-            )))}
-        </div>
-    
-        const childrenCause = (
-        <div className='flex flex-col'>
-          <h4 className="font-bold mt-2">{causes[0].text}</h4>
-          <hr className="border-t border-gray-400 my-2 mx-20"></hr>
-          <div
-            className="flex h-32 w-2/3 self-center bg-cover rounded-lg bg-no-repeat mb-4"
-            style={{ backgroundImage: `url(${causes[0].image})` }}
-          ></div>
-        </div>
-    )
-    const MWCause = (
-        <div className='flex flex-col'>
-          <h4 className="font-bold mt-2">{causes[1].text}</h4>
-          <hr className="border-t border-gray-400 my-2 mx-20"></hr>
-          <div
-            className="flex h-32 w-2/3 self-center bg-cover rounded-lg bg-no-repeat mb-4"
-            style={{ backgroundImage: `url(${causes[1].image})` }}
-          ></div>
-        </div>
-    )
-    const DPCause = (
-        <div className='flex flex-col'>
-          <h4 className="font-bold mt-2">{causes[2].text}</h4>
-          <hr className="border-t border-gray-400 my-2 mx-20"></hr>
-          <div
-            className="flex h-32 w-2/3 self-center bg-cover rounded-lg bg-no-repeat mb-4"
-            style={{ backgroundImage: `url(${causes[2].image})` }}
-          ></div>
-        </div>
-    ) */}
