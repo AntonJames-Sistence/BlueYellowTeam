dd4e64b6c4d5846e07791220e04c9f8a235c5377
James <james.hernandez1225@gmail.com>
2023-12-03 19:30:55 -0500

    Added  firebase adapater to nextauth

--- app/api/auth/[...nextauth]/options.js
+++ app/api/auth/[...nextauth]/options.js
@@ -1,4 +1,6 @@
 import GoogleProvider from "next-auth/providers/google";
+import { FirestoreAdapter } from "@auth/firebase-adapter";
+import { cert } from "firebase-admin/app";
 
 export const authOptions = {
   secret: process.env.NEXTAUTH_SECRET,
@@ -8,6 +10,13 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  adapter: FirestoreAdapter({
+    credential: cert({
+      projectId: "blueyellowfoundation-b5284",
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    }),
+  }),
   callbacks: {
     async signIn({ user, account, profile, email, credentials }) {
       // console.log("session", user, account, profile, email, credentials);
