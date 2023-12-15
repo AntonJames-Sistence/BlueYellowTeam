1bf1f836494b4c802b71ec4622be7199513df351
AntonJames-Sistence <anton.james.ja@gmail.com>
2023-12-15 09:49:40 -0500

    Create test endpoint

---
+++ app/(pages)/fb-test/page.js
@@ -0,0 +1,7 @@
+import FacebookTest from "../../components/facebook_test";
+
+export default function ProjectsPage() {
+    return (
+      <FacebookTest />
+    );
+  }
--- app/components/facebook_test.jsx
+++ app/components/facebook_test.jsx
@@ -1,19 +1,22 @@
-import React, { useEffect } from 'react';
+'use client';
+import React, { useEffect, useState } from 'react';
 
 const FacebookTest = () => {
+  const [groupData, setGroupData] = useState(null); // State to store fetched group data
+
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const accessToken = 'YOUR_FACEBOOK_ACCESS_TOKEN';
-        const groupId = 'BlueYellowFoundation'; // Group ID or Name
+        const accessToken = 'EAAFRjOfy5J4BO2iHZCgQSCRucuNg6JRN06EBhFWZB9z4g3dsP7lquOQ0nm7ORgAEr7uFhPgScfmza9q0lr2cwA3pW6eFzad3gsvGEqCS3xzxLxDnh0Wv0qT520O3AKrUJnPxZBKwycFZCsZAhOl44FfBvwkgnRWNKC8UCSx2nF1ZCEkduh3EZBycTMR';
+        const groupId = '101182722724503';
 
         const response = await fetch(
-          `https://graph.facebook.com/v13.0/${groupId}?access_token=${accessToken}`
+          `https://graph.facebook.com/v13.0/${groupId}/feed?access_token=${accessToken}`
         );
 
         if (response.ok) {
           const data = await response.json();
-          console.log(data); // Log the retrieved data
+          setGroupData(data); // Set the fetched data to the state
         } else {
           console.error('Error fetching data:', response.statusText);
         }
@@ -27,7 +30,19 @@ const FacebookTest = () => {
 
   return (
     <div>
-      {/* Your component JSX */}
+      <h2>Facebook Group Wall</h2>
+      {groupData ? (
+        <div>
+          {groupData.data.map((post) => (
+            <div key={post.id}>
+              <p>{post.message}</p>
+              {/* You can display other post details as needed */}
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 };
