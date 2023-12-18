4f606cfebaaa0107311ca921f4a04a3ff0feada1
AntonJames-Sistence <anton.james.ja@gmail.com>
2023-12-18 16:15:50 -0500

    Test for production

--- app/api/webhook/route.js
+++ app/api/webhook/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
 
 export async function POST(request) {
-  if (request.method === 'POST') {
+  
     const sig = request.headers['stripe-signature'];
     let event;
 
@@ -12,23 +12,35 @@ export async function POST(request) {
       // Sending a response back to the frontend
       // return NextResponse.json({ message: 'hi from backend' });
     } catch (error) {
-      return NextResponse.status(400).end(`Webhook Error: ${err.message}`);
+      return NextResponse.json({
+        message: `Webhook Error: ${error.message}`
+      }, {
+        status: 400,
+      });
     }
 
     // Handle the event
     switch (event.type) {
-      case 'checkout.session.completed':
+      case 'payment_intent.succeeded':
         const paymentIntentSucceeded = event.data.object;
-        // Define and call a function to handle the event payment_intent.succeeded
-        // Perform necessary operations with paymentIntentSucceeded
+        // let customerEmail = paymentIntentSucceeded.receipt_email;
+        
         break;
-      // ... handle other event types
+
+      case 'checkout.session.completed':
+        const checkoutSessionCompleted = event.data.object;
+        let customerEmail = checkoutSessionCompleted.receipt_email;
+
+        return NextResponse.json({ message: 'customerEmail' });
+        // sendThankYouEmail(customerEmail);
+
+        break;
+
       default:
         console.log(`Unhandled event type ${event.type}`);
     }
 
-    return NextResponse.status(200).end();
-  } else {
-    return NextResponse.status(405).end('Method Not Allowed');
-  }
+    return NextResponse.json({
+      status: 200,
+    });
 };
