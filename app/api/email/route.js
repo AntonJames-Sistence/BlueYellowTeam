c8f0a98e20f486ffce4cb57ee134c96801a98018
AntonJames-Sistence <anton.james.ja@gmail.com>
2023-12-19 11:06:49 -0500

    Change auth settings for nodemailer

--- app/api/email/route.js
+++ app/api/email/route.js
@@ -6,11 +6,15 @@ export async function POST(req) {
 
     try {
         const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: process.env.GMAIL_LOGIN,
-            pass: process.env.GMAIL_PASSWORD,
-        },
+            service: 'gmail',
+            auth: {
+              type: 'OAuth2',
+              user: process.env.MAIL_USERNAME,
+              pass: process.env.MAIL_PASSWORD,
+              clientId: process.env.OAUTH_CLIENTID,
+              clientSecret: process.env.OAUTH_CLIENT_SECRET,
+              refreshToken: process.env.OAUTH_REFRESH_TOKEN
+            }
         });
 
         const mailOptions = {
