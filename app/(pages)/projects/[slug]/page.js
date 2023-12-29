71dfcf725c78e3d8323ea2ab4649ebd00d77c551
AntonJames-Sistence <anton.james.ja@gmail.com>
2023-12-29 14:09:51 -0500

    Add responsive design to Projects

--- app/(pages)/projects/[slug]/page.js
+++ app/(pages)/projects/[slug]/page.js
@@ -7,7 +7,7 @@ export default function ProjectIndexPage({ params: { slug } }) {
   const project = allProjects.find((project) => project.slug === slug);
 
   return (
-    <div className="flex justify-center w-full mt-12">
+    <div className="flex justify-center w-full mt-6">
       <div className="mx-4 lg:mx-0">
         <Button
             css="text-gray-800 px-4 py-2 rounded-full text-sm md:text-base font-bold transition-colors duration-200 bg-yellow-400 hover:bg-yellow-500"
@@ -15,15 +15,20 @@ export default function ProjectIndexPage({ params: { slug } }) {
             url="/projects"
         />
         <div className="max-w-[1000px]">
-          <div className="text-3xl text-center lg:text-left lg:text-5xl text-bold my-4">{project.title}</div>
-          <Image 
-            className="object-cover" 
-            src={`${project.img}`} 
-            width={1000}
-            height={10}
-          />
-          <div className="text-sm lg:text-xl">{project.date} 30.10</div>
-          <div className="text-base lg:text-2xl mt-4">{project.para}</div>
+          <div className="text-3xl text-center md:text-left lg:text-5xl text-bold my-4">{project.title}</div>
+          <div className="flex flex-col md:flex-row">
+            <Image 
+              className="object-cover rounded-xl md:w-1/2" 
+              src={`${project.img}`} 
+              width={500}
+              height={0}
+            />
+            <div className="mt-4 md:ml-4 md:mt-0 flex flex-col justify-between">
+              <div className="text-base md:text-xl">{project.para}</div>
+              <div className="text-sm lg:text-xl mt-2 font-bold">{project.date}01.30.2023</div>
+            </div>
+            </div>
+          
         </div>
       </div>
     </div>
