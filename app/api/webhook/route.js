4d34cf273d61bf29c6fcf69aaec58fdef095d64c
AntonJames-Sistence <anton.james.ja@gmail.com>
2023-12-29 16:46:23 -0500

    Live version

--- app/api/webhook/route.js
+++ app/api/webhook/route.js
@@ -2,8 +2,8 @@ import Stripe from 'stripe';
 import { buffer } from "node:stream/consumers";
 import { NextResponse } from 'next/server';
 
-const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET_LIVE;
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY_LIVE);
 
 export async function POST(request) {
   const rawBody = await buffer(request.body);
