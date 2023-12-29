5e6f022ca2d7b9725b211474d42dd455896ee3f1
AntonJames-Sistence <anton.james.ja@gmail.com>
2023-12-29 13:51:38 -0500

    Fix style for project slug

--- app/(pages)/projects/[slug]/page.js
+++ app/(pages)/projects/[slug]/page.js
@@ -2,22 +2,29 @@ import { allProjects } from "../../../../data/projects";
 import PageHeader from "../../components/PageHeader";
 import "./style.css";
 import Button from "../../../components/Button";
+import Image from "next/image";
 
 export default function ProjectIndexPage({ params: { slug } }) {
   const project = allProjects.find((project) => project.slug === slug);
+
   return (
-    <div className="flex justify-center w-full py-10">
-      <div>
+    <div className="flex justify-center w-full mt-12">
+      <div className="mx-4 lg:mx-0">
         <Button
             css="text-gray-800 px-4 py-2 rounded-full text-sm md:text-base font-bold transition-colors duration-200 bg-yellow-400 hover:bg-yellow-500"
             text="« All Projects"
             url="/projects"
         />
-        <div id="project-cont">
-          <div id="title">{project.title}</div>
-          <div id="date">{project.date}</div>
-          <img id="img" src={`..${project.img}`}></img>
-          <div id="para">{project.para}</div>
+        <div className="max-w-[1000px]">
+          <div className="text-3xl text-center lg:text-left lg:text-5xl text-bold my-4">{project.title}</div>
+          <Image 
+            className="object-cover" 
+            src={`${project.img}`} 
+            width={1000}
+            height={10}
+          />
+          <div className="text-sm lg:text-xl">{project.date} 30.10</div>
+          <div className="text-base lg:text-2xl mt-4">{project.para}</div>
         </div>
       </div>
     </div>
