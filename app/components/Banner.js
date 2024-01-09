8ab67c272de3f01f62ea67a223ff527bf124074c
AntonJames-Sistence <anton.james.ja@gmail.com>
2024-01-09 17:39:14 -0500

    Change banner img to old one

--- app/components/Banner.js
+++ app/components/Banner.js
@@ -36,16 +36,16 @@ const Banner = () => {
         ></video>
         <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-between md:flex-row">
           <div className="w-full lg:-mt-10 md:-mt-14 mt-4 md:w-1/2 flex flex-col justify-center p-5 absolute md:static top-1/2 -translate-y-1/2 md:top-0 md:translate-y-0">
-            <div className="uppercase text-4xl md:text-3xl xl:text-4xl text-center md:text-left text-white font-bold text-shadow-lg -mt-8 md:pb-2 md:mt-4 lg:-mt-6">
+            <div className="uppercase text-2xl md:text-3xl xl:text-4xl text-center md:text-left text-white font-bold text-shadow-lg -mt-8 md:pb-2 md:mt-4 lg:-mt-6">
               Resilience and bravery in times of war
             </div>
-            <div className="uppercase font-semibold text-white md:-mb-6 text-center md:text-left mt-4 text-2xl md:text-lg xl:text-xl">
-              100% of donations go to support ukraine through trusted funds or
+            <div className="uppercase font-semibold text-white md:-mb-6 text-center md:text-left mt-4 text-lg xl:text-xl">
+              100% of your donation supports ukraine through trusted funds or
               direct aid
             </div>
           </div>
           <div
-            className="w-full md:w-3/4 h-1/2 md:h-full lg:h-full bg-cover bg-no-repeat rounded-2xl hidden md:block bg-[url('/girl.png')]"
+            className="w-full md:w-3/4 h-1/2 md:h-full lg:h-full bg-cover bg-no-repeat rounded-2xl hidden md:block bg-[url('/intro_banner.png')]"
             
           ></div>
         </div>
