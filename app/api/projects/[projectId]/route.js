b595a2b88c35d16d2d285893a6b7fb10a7740522
James <james.hernandez1225@gmail.com>
2024-01-09 23:54:48 -0500

    created full CRUD backend routes for projects feature

---
+++ app/api/projects/[projectId]/route.js
@@ -0,0 +1,79 @@
+import { NextResponse, NextRequest } from "next/server";
+import {
+  collection,
+  setDoc,
+  doc,
+  addDoc,
+  getDoc,
+  getDocs,
+  updateDoc,
+  deleteDoc,
+} from "firebase/firestore";
+import { storeDB } from "../../../../data/firebase";
+import { isAdmin, projectHasErrors } from "../../lib";
+
+export async function GET(request, { params: { projectId } }) {
+  try {
+    const postRef = await getDoc(doc(storeDB, "projects", projectId));
+
+    if (postRef.exists()) {
+      const post = { ...postRef.data() };
+      return NextResponse.json(post);
+    }
+
+    return NextResponse.json(
+      { errors: "Couldn't retreieve project" },
+      { status: 400 }
+    );
+  } catch (error) {
+    return NextResponse.error("Couldn't get project");
+  }
+}
+
+export async function PUT(request, { params: { projectId } }) {
+  try {
+    // await isAdmin();
+    const body = await request.json();
+
+    if (projectHasErrors(body)) {
+      return NextResponse.json(
+        { error: "Project was missing components" },
+        400
+      );
+    }
+
+    const postRef = doc(storeDB, "projects", body.id);
+    const realPost = await getDoc(postRef);
+
+    if (!realPost.exists()) {
+      return NextResponse.json(
+        { erros: "This blog does not exist" },
+        { status: 404 }
+      );
+    }
+
+    const update = {
+      description: body.description,
+    };
+
+    if (body?.image.startsWith("https://drive.google.com/file/d/")) {
+      update.image = createGoogleDriveLink(post.image);
+    }
+
+    await updateDoc(postRef, update);
+
+    return NextResponse.json("Project successfully updated");
+  } catch (error) {
+    return NextResponse.error(error.message);
+  }
+}
+
+export async function DELETE(request, { params: { projectId } }) {
+  try {
+    // await isAdmin();
+    await deleteDoc(doc(storeDB, "projects", projectId));
+    return NextResponse.json("successfully deleted");
+  } catch (error) {
+    return NextResponse.error("couldn't delete post");
+  }
+}
