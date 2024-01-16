649e3aafd1309b451fe89b8fb19f9b9ee41f6642
AntonJames-Sistence <anton.james.ja@gmail.com>
2024-01-16 15:09:05 -0500

    Bug fix

--- app/components/UpdateFacebookBtn.js
+++ app/components/UpdateFacebookBtn.js
@@ -1,11 +1,14 @@
 "use client";
-import React from "react";
+import { React, useState } from "react";
 import { useSession } from "next-auth/react";
 import toast, { Toaster } from "react-hot-toast";
 
 export default function UpdateFacebookBtn() {
+  const [ isLoading, setIsLoading ] = useState(false);
   const session = useSession();
+
   const handleClick = async () => {
+    setIsLoading(true);
     const res = await fetch("api/facebook", {
       method: "PUT",
     });
@@ -15,15 +18,31 @@ export default function UpdateFacebookBtn() {
     } else {
       toast.error("Failed to update facebook post");
     }
+    setIsLoading(false);
   };
   if (!session?.data) return;
   return (
-    <div
-      className="bg-yellow-400 font-semibold px-3 py-1 w-fit rounded-md cursor-pointer hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 transition-colors duration-200 mb-4"
-      onClick={handleClick}
-    >
-      <Toaster position="top-right" reverseOrder={false} />
-      Update Facebook post
-    </div>
+    isLoading ?
+    (
+      <div className="flex items-center justify-center h-[10vh]">
+        <div id="load" className="text-3xl font-bold text-shadow-lg">
+          <div>G</div>
+          <div>N</div>
+          <div>I</div>
+          <div>D</div>
+          <div>A</div>
+          <div>O</div>
+          <div>L</div>
+        </div>
+      </div>
+    ) : (
+      <div
+        className="bg-yellow-400 font-semibold px-3 py-1 w-fit rounded-md cursor-pointer hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 transition-colors duration-200 mb-4"
+        onClick={handleClick}
+      >
+        <Toaster position="top-right" reverseOrder={false} />
+        Update Facebook post
+      </div>
+    )
   );
 }
